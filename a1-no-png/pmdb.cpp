7d1ca453cda6fc41f379c7e4077f594eda62a7db
ged <nesalang@gmail.com>
2013-01-22 23:07:07 -0800

    "block partition added"

--- a1-no-png/pmdb.cpp
+++ a1-no-png/pmdb.cpp
@@ -31,19 +31,20 @@ int ComputeMandelbrotPoint(int x, int y, int dimX, int dimY);
 void* fill_array_with_mandelbrotpoint(void* arg)
 {
     ThreadArgs *args = (ThreadArgs*) arg;
-
-    for(int i = 0; i < args->height; i++)
+    if(-1 != args->start_x)
     {
-        for(int j = 0; j < args->width; j++)
+        for(int i = 0; i < args->height; i++)
         {
-            int x = args->start_x + j;
-            int y = args->start_y + i;
+            for(int j = 0; j < args->width; j++)
+            {
+                int x = args->start_x + j;
+                int y = args->start_y + i;
 
-            args->plot[x][y] =  
-                ComputeMandelbrotPoint (x, y, 0, 0);
+                args->plot[x][y] =  
+                    ComputeMandelbrotPoint (x, y, 0, 0);
+            }
         }
     }
-
     return NULL;
 }
 
@@ -65,18 +66,90 @@ void Mandelbrot_pthreads(int** pts, int dimX, int dimY, int numThreads, int chun
     ThreadArgs* args = new ThreadArgs[nThreads]; 
 
     // thread creation
-    for(int t=0; t<nThreads; t++)
+    // for block partition, there will be alway four blocks
+    // the four blocks are defined as a retangular with
+    // following dimensions: dimY/4 X dimX
+    // Each thread will be given a number of block 
+    // untill all threads have work
+    // if there is one thread all work is givne the thread
+    // if there is two threads each thread gets the half
+    // if there is three threads each thread get one
+    // and the first one get one more
+    // if there are four threads
+    // each gets one
+    // threads more than 4 do not receive anywork
+    
+    switch(nThreads)
     {
-        
+        case 1:
+            args[0].start_x = 0;
+            args[0].start_y = 0;
+            args[0].width   = dimX; 
+            args[0].height  = dimY;; 
+            break;
+        case 2:
+            args[0].start_x = 0;
+            args[0].start_y = 0;
+            args[0].width   = dimX; 
+            args[0].height  = dimY/ 2; 
+
+            args[1].start_x = 0;
+            args[1].start_y = dimY;
+            args[1].width   = dimX; 
+            args[1].height  = dimY/ 2; 
+            break;    
+        case 3:
+            args[0].start_x = 0;
+            args[0].start_y = 0;
+            args[0].width   = dimX; 
+            args[0].height  = dimY / 4; 
+
+            args[1].start_x = 0;
+            args[1].start_y = dimY / 4;
+            args[1].width   = dimX; 
+            args[1].height  = dimY / 4; 
+
+            args[2].start_x = 0;
+            args[2].start_y = dimY / 2;
+            args[2].width   = dimX; 
+            args[2].height  = dimY/2; 
+            break;
+        case 4: case 5: case 6: 
+        case 7: case 8:
+            args[0].start_x = 0;
+            args[0].start_y = 0;
+            args[0].width   = dimX; 
+            args[0].height  = dimY / 4; 
+
+            args[1].start_x = 0;
+            args[1].start_y = dimY / 4;
+            args[1].width   = dimX; 
+            args[1].height  = dimY / 4; 
+
+            args[2].start_x = 0;
+            args[2].start_y = dimY / 2;
+            args[2].width   = dimX; 
+            args[2].height  = dimY / 4; 
+
+            args[3].start_x = 0;
+            args[3].start_y = dimY / 3;
+            args[3].width   = dimX; 
+            args[3].height  = dimY / 4; 
+            break;    
+    }
+
+    for(int t = 0; t < nThreads; t++)
+    {
         args[t].tid = t; 
         args[t].NT = nThreads; 
-
-        // we need to figure out how to partition the plot spaces 
-        // to each threads 
-        args[t].start_x = 0;
-        args[t].start_y = 0;
-        args[t].width   = 0; 
-        args[t].height  = 0; 
+        args[t].plot = pts;
+        if(t > 3)
+        {
+            args[t].start_x = -1;
+            args[t].start_y = -1;
+            args[t].width   = -1;
+            args[t].height  = -1;
+        }
 
         assert(!pthread_create(&threads[t], NULL, fill_array_with_mandelbrotpoint, &args[t]));
     }
