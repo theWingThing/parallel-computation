682ec8d9be57e6ba18c491758db2f0b6378e70a5
Sharon Kuo <nar@Sharons-MacBook-Air.local>
2013-01-24 21:56:33 -0800

    corner cases

--- a1-no-png/pmdb.cpp
+++ a1-no-png/pmdb.cpp
@@ -3,6 +3,9 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <iostream>
+
+using namespace std;
 
 // holds thread arguments
 struct ThreadArgs
@@ -10,10 +13,12 @@ struct ThreadArgs
     int tid;          // thread id
     int NT;           // number of thread
     int** plot;       // mandelbrot points storage
-    int start_x;      // start coordinate of x
-    int start_y;      // start coordinate of y
     int64_t width;    // horizontal witdh
     int64_t height;   // horizontal height
+    int* job_queue;   // contains index to the first row of each chunk queued
+    int queue_size;   // num of elements in the array
+    int chunk_size;   // size of chunks in number of rows
+    int special;      // if this is the last chunk of an uneven input
 };
 
 // Implemented for you in smdb.h
@@ -31,16 +36,24 @@ int ComputeMandelbrotPoint(int x, int y, int dimX, int dimY);
 void* fill_array_with_mandelbrotpoint(void* arg)
 {
     ThreadArgs *args = (ThreadArgs*) arg;
-    if(-1 != args->start_x)
-    {
-        for(int i = 0; i < args->height; i++)
-        {
-            for(int j = 0; j < args->width; j++)
-            {
-                int x = args->start_x + j;
-                int y = args->start_y + i;
 
-                args->plot[x][y] =  
+    for(int k = 0; k < args->queue_size; k++)
+    {
+        int start_y = args->job_queue[k];
+        int height  = args->chunk_size;
+	if(args->special && k == args->queue_size-1)
+	    height  = args->special;
+        int start_x = 0;
+        int width   = args->width;
+
+        for(int i = 0; i < height; i++)
+        {
+            for(int j = 0; j < width; j++)
+            {
+                int x = start_x + j;
+                int y = start_y + i;
+
+                args->plot[y][x] =  
                     ComputeMandelbrotPoint (x, y, 0, 0);
             }
         }
@@ -50,10 +63,6 @@ void* fill_array_with_mandelbrotpoint(void* arg)
 
 void Mandelbrot_pthreads(int** pts, int dimX, int dimY, int numThreads, int chunkSize) 
 {
-    //////////// add your code here //////////////////////////////////
-    //// creates #numThreads of threads
-    //// for test just evenly divde the works among the threads
-    
     if(1 > numThreads)
     {
         fprintf(stderr, "invalid argument of numThreads = %d\n", numThreads);
@@ -64,106 +73,71 @@ void Mandelbrot_pthreads(int** pts, int dimX, int dimY, int numThreads, int chun
 
     pthread_t* threads = new pthread_t[nThreads]; 
     ThreadArgs* args = new ThreadArgs[nThreads]; 
-
-    // thread creation
-    // for block partition, there will be alway four blocks
-    // the four blocks are defined as a retangular with
-    // following dimensions: dimY/4 X dimX
-    // Each thread will be given a number of block 
-    // untill all threads have work
-    // if there is one thread all work is givne the thread
-    // if there is two threads each thread gets the half
-    // if there is three threads each thread get one
-    // and the first one get one more
-    // if there are four threads
-    // each gets one
-    // threads more than 4 do not receive anywork
     
-    for(int k = 0; k < nThreads; k++){
-        args[k].start_x = 0;
-        args[k].start_y = k/nThreads;
-        args[k].width = dimX;
-        args[k].height = (k+1)/nThreads;
-    }
-/*
-    switch(nThreads)
+    if(chunkSize)
     {
-        case 1:
-            args[0].start_x = 0;
-            args[0].start_y = 0;
-            args[0].width   = dimX; 
-            args[0].height  = dimY; 
-            break;
-        case 2:
-            args[0].start_x = 0;
-            args[0].start_y = 0;
-            args[0].width   = dimX; 
-            args[0].height  = dimY/ 2; 
-
-            args[1].start_x = 0;
-            args[1].start_y = dimY;
-            args[1].width   = dimX; 
-            args[1].height  = dimY/ 2; 
-            break;    
-        case 3:
-            args[0].start_x = 0;
-            args[0].start_y = 0;
-            args[0].width   = dimX; 
-            args[0].height  = dimY / 4; 
-
-            args[1].start_x = 0;
-            args[1].start_y = dimY / 4;
-            args[1].width   = dimX; 
-            args[1].height  = dimY / 4; 
-
-            args[2].start_x = 0;
-            args[2].start_y = dimY / 2;
-            args[2].width   = dimX; 
-            args[2].height  = dimY/2; 
-            break;
-        default:
-            args[0].start_x = 0;
-            args[0].start_y = 0;
-            args[0].width   = dimX; 
-            args[0].height  = dimY / 4; 
-
-            args[1].start_x = 0;
-            args[1].start_y = dimY / 4;
-            args[1].width   = dimX; 
-            args[1].height  = dimY / 4; 
-
-            args[2].start_x = 0;
-            args[2].start_y = dimY / 2;
-            args[2].width   = dimX; 
-            args[2].height  = dimY / 4; 
-
-            args[3].start_x = 0;
-            args[3].start_y = dimY / 3;
-            args[3].width   = dimX; 
-            args[3].height  = dimY / 4; 
-            break;    
-    }
-*/
-    for(int t = 0; t < nThreads; t++)
-    {
-        args[t].tid = t; 
-        args[t].NT = nThreads; 
-        args[t].plot = pts;
-        if(t > 3)
+        for(int k = 0; k < nThreads; k++)
         {
-            args[t].start_x = -1;
-            args[t].start_y = -1;
-            args[t].width   = -1;
-            args[t].height  = -1;
-        }
+	    int extra = (dimY/chunkSize)%numThreads;
+            int queue_size;
+	    if(k < extra || (dimY % chunkSize != 0 && k == extra))
+		queue_size = (dimY / (chunkSize * numThreads))+1;
+	    else
+		queue_size = dimY / (chunkSize * numThreads);
+cerr << "Thread " << k << " has queue size " << queue_size <<endl;
+            /*
+            if(dimY % (chunkSize * numThreads) && (nThreads - 1 == k))
+            { 
+                queue_size++;
+            }
+            */
 
-        assert(!pthread_create(&threads[t], NULL, fill_array_with_mandelbrotpoint, &args[t]));
+            args[k].job_queue = (int*) malloc(sizeof(int)*(queue_size)); 
+            assert(args[k].job_queue);
+
+            args[k].queue_size = queue_size;
+
+            for(int i = 0; i < queue_size; i++)
+            {
+                args[k].job_queue[i] = k * (chunkSize) + (chunkSize*nThreads) * i;
+		if(i == queue_size - 1 && dimY % chunkSize != 0)
+		    args[k].special = dimY % chunkSize;
+		else
+		    args[k].special = 0;
+cerr << "Storing job " << args[k].job_queue[i] << endl;
+            }
+            args[k].width = dimX;
+            args[k].chunk_size = chunkSize;
+            args[k].tid = k; 
+            args[k].NT = nThreads; 
+            args[k].plot = pts;
+            assert(!pthread_create(&threads[k], NULL, fill_array_with_mandelbrotpoint, &args[k]));
+cerr << "got here" << endl;
+        }
     }
+    else
+    {
+        for(int k = 0; k < nThreads; k++)
+        {
+            args[k].job_queue = (int*) malloc(sizeof(int)); 
+            assert(args[k].job_queue);
+            args[k].queue_size = 1;
+            args[k].job_queue[0] = (dimY/nThreads)*k;
+            args[k].width = dimX;
+            args[k].chunk_size = (dimY/nThreads);
+            args[k].tid = k; 
+            args[k].NT = nThreads; 
+            args[k].plot = pts;
+            assert(!pthread_create(&threads[k], NULL, fill_array_with_mandelbrotpoint, &args[k]));
+        }
+    }
+
 
     for(int t=0; t < nThreads; t++)
     {
         void *value;
         pthread_join(threads[t], &value);
+        free(args[t].job_queue);
     }
 }
 
